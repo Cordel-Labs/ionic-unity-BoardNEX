40f63f1cdc4b52e0837e9597ffbe3eb377590962
acferM <promacfer@gmail.com>
2022-04-25 15:57:24 -0300

    added ui transitions

---
+++ Assets/Scripts/UI/PanelsNavigation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelsNavigation : MonoBehaviour
+{
+    private int currentPanel = 0;
+    [SerializeField] GameObject[] panels;
+
+    public void confirm()
+    {
+        panels[currentPanel].GetComponent<Animator>().SetBool("active", false);
+        var hasNext = currentPanel == panels.Length - 1;
+        currentPanel = hasNext ? 0 : currentPanel + 1;
+        panels[currentPanel].GetComponent<Animator>().SetBool("active", true);
+    }
+    
+    public void delete()
+    {
+        if (currentPanel < 1) return;
+
+        panels[currentPanel].GetComponent<Animator>().SetBool("active", false);
+        currentPanel--;
+        panels[currentPanel].GetComponent<Animator>().SetBool("active", true);
+    }
+}
