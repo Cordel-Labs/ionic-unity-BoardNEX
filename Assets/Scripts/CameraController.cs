44a2e51c1d0bac4c5f89ba2160585a337663f955
acferM <promacfer@gmail.com>
2022-04-28 17:04:44 -0300

    mouse camera controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,8 +5,50 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private bool isDragging = false;
+    private float mouseStartX, mouseStartY;
+        
     void Update()
     {
+        if (Input.mouseScrollDelta.y > 0 && Camera.main.orthographicSize > 1)
+        {
+            Camera.main.orthographicSize -= .5f;
+        }
+        else if (Input.mouseScrollDelta.y < 0 && Camera.main.orthographicSize < 21)
+        {
+            Camera.main.orthographicSize += .5f;
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            isDragging = true;
+            mouseStartX = Input.mousePosition.x;
+            mouseStartY = Input.mousePosition.y;
+        }
+        
+        if (Input.GetMouseButtonUp(2))
+        {
+            isDragging = false;
+        }
+
+        if (isDragging)
+        {
+            if (Input.mousePosition.x != mouseStartX)
+            {
+                var difference = (Input.mousePosition.x - mouseStartX) / 80;
+                var positionUpdate = difference > 0 ? transform.position.x - difference : transform.position.x + -difference;
+                transform.position = new Vector3(positionUpdate, transform.position.y, -10);
+                mouseStartX = Input.mousePosition.x;
+            }
+            
+            if (Input.mousePosition.y != mouseStartY)
+            {
+                var difference = (Input.mousePosition.y - mouseStartY) / 80;
+                var positionUpdate = difference > 0 ? transform.position.y - difference : transform.position.y + -difference;
+                transform.position = new Vector3(transform.position.x, positionUpdate, -10);
+                mouseStartY = Input.mousePosition.y;
+            }
+        }
     }
 
     public void Zoom(string type)
