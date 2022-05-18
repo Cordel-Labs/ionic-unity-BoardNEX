6c32f9e4a85debf73f377fee74000501c00950b6
acferM <promacfer@gmail.com>
2022-05-18 13:10:40 -0300

    Created menu screen ui

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public void EnterBoard() {}
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
